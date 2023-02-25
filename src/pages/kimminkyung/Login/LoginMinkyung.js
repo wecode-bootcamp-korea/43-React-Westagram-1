dfb389a873c3fa93d7732ec2af86ee80900914b4
김민경 <gimmingyeong@gimmingyeong-ui-MacBookPro.local>
2023-02-25 17:23:51 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/kimminkyung/Login/LoginMinkyung.js
+++ src/pages/kimminkyung/Login/LoginMinkyung.js
@@ -1,7 +1,46 @@
 import React from 'react';
-//rafce
-const LoginMinkyung = () => {
-  return <div>Login</div>;
-};
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './LoginMinkyung.scss';
 
+const LoginMinkyung = () => {
+  const navigate = useNavigate();
+  const goToMain = () => {
+    navigate('/main');
+  };
+
+  const [id, setId] = useState('');
+  const saveUserId = e => {
+    setId(e.target.value);
+  };
+
+  const [pw, setPw] = useState('');
+  const saveUserPw = e => {
+    setPw(e.target.vlaue);
+  };
+
+  return (
+    <div className="loginminkyung">
+      <header className="logo">Westagram</header>
+      <div className="container">
+        <input
+          value={id}
+          onChange={saveUserId}
+          className="id"
+          placeholder="전화번호,사용자 이름 또는 이메일"
+        />
+        <input
+          value={pw}
+          onChange={saveUserPw}
+          className="password"
+          placeholder="비밀번호"
+        />
+        <button disal onClick={goToMain} className="account">
+          로그인
+        </button>
+      </div>
+      <p className="bottom">비밀번호를 잊으셨나요?</p>
+    </div>
+  );
+};
 export default LoginMinkyung;
