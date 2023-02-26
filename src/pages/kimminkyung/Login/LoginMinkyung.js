aaaa10bc8a787b8d66f4f5018ce627434ddb6913
김민경 <gimmingyeong@gimmingyeong-ui-MacBookPro.local>
2023-02-26 13:52:39 +0900

    Add : Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/kimminkyung/Login/LoginMinkyung.js
+++ src/pages/kimminkyung/Login/LoginMinkyung.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
+
 import { useNavigate } from 'react-router-dom';
 import './LoginMinkyung.scss';
 
@@ -8,7 +8,7 @@ const LoginMinkyung = () => {
   const goToMain = () => {
     navigate('/main');
   };
-
+  /*1-1번 */
   const [id, setId] = useState('');
   const saveUserId = e => {
     setId(e.target.value);
@@ -16,29 +16,51 @@ const LoginMinkyung = () => {
 
   const [pw, setPw] = useState('');
   const saveUserPw = e => {
-    setPw(e.target.vlaue);
+    setPw(e.target.value);
   };
+  /*1-2번 */
+  // const [isActive, setIsActive] = useState('account');
+  // const check = () => {
+  //   id.includes('@') && pw.length > 4
+  //     ? setIsActive('active')
+  //     : setIsActive('account');
+  // };
+  const [isActive, setIsActive] = useState(false);
+  const check = () => {
+    id.includes('@') && pw.length > 4 ? setIsActive(true) : setIsActive(false);
+  };
+  //const check = () => {
+  // setId.includes('@') && pw.length > 4 ? setIsActive('active') : setIsActive('account') }
 
   return (
-    <div className="loginminkyung">
-      <header className="logo">Westagram</header>
-      <div className="container">
-        <input
-          value={id}
-          onChange={saveUserId}
-          className="id"
-          placeholder="전화번호,사용자 이름 또는 이메일"
-        />
-        <input
-          value={pw}
-          onChange={saveUserPw}
-          className="password"
-          placeholder="비밀번호"
-        />
-        <button disal onClick={goToMain} className="account">
+    <div onKeyUp={check} className="loginminkyung">
+      <form>
+        <header className="logo">Westagram</header>
+        <div className="container">
+          <input
+            value={id}
+            onChange={saveUserId}
+            className="id"
+            placeholder="전화번호,사용자 이름 또는 이메일"
+          />
+          <input
+            value={pw}
+            onChange={saveUserPw}
+            className="password"
+            placeholder="비밀번호"
+            type="password"
+          />
+          {/* <button onClick={goToMain} className={isActive}>
           로그인
-        </button>
-      </div>
+        </button> */}
+          <button
+            className={`account ${isActive ? 'active' : ''}`}
+            onClick={goToMain}
+          >
+            로그인
+          </button>
+        </div>
+      </form>
       <p className="bottom">비밀번호를 잊으셨나요?</p>
     </div>
   );
