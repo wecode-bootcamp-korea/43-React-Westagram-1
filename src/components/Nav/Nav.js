b5fe6a71c3bc5576efca845b27fa85c0664b02e5
Dante6327 <jtgk6327@hanmail.net>
2023-02-28 17:41:31 +0900

    ADD : 검색 기능 추가

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Nav.scss';
 import instaImg from 'assets/common/instagram.png';
 import compassImg from 'assets/common/compass.png';
 import emptyHeartImg from 'assets/common/emptyHeart.png';
 import profileImg from 'assets/common/account.png';
 
-const Nav = () => {
+const Nav = props => {
+  const { userList } = props;
+  const [inputText, setInputText] = useState('');
+  const [selectedUser, setSelectedUser] = useState([]);
+  const [viewSearchArea, setViewSearchArea] = useState(false);
+
+  useEffect(() => {
+    if (inputText) {
+      setSelectedUser(userList.filter(u => u.includes(inputText)));
+      setViewSearchArea(true);
+    } else {
+      setSelectedUser([]);
+      setViewSearchArea(false);
+    }
+  }, [inputText, userList]);
+  const handleChange = e => {
+    setInputText(e.target.value);
+  };
+
   return (
     <div className="nav">
       <div className="home">
@@ -13,8 +31,26 @@ const Nav = () => {
         <span className="txtHome">Westagram !!</span>
       </div>
       <div className="search">
-        <input className="txtSearch" type="text" placeholder="검색" />
+        <input
+          className="txtSearch"
+          type="text"
+          placeholder="검색"
+          onChange={handleChange}
+          onBlur={() => setViewSearchArea(false)}
+          value={inputText}
+        />
+        {viewSearchArea && (
+          <div className="searchArea">
+            {selectedUser.map((u, idx) => (
+              <div key={idx} className="searchUserOne">
+                <button className="searchUserIcon" />
+                {u}
+              </div>
+            ))}
+          </div>
+        )}
       </div>
+
       <div className="navList">
         <ul>
           <li>
