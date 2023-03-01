3444bdd9cccc3c5bd46f9098b442771937d161f6
진평 김 <jinp12@playauto.co.kr>
2023-03-01 17:58:43 +0900

    MODIFY : 로그인, 메인페이지 css 오작용으로 클래스네임 수정

--- src/pages/kimjinpyeong/Main/MainJinpyeong.js
+++ src/pages/kimjinpyeong/Main/MainJinpyeong.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './MainJinpyeong.scss';
 import Nav from 'components/Nav/Nav';
 import Feeds from './mainComponents/Feeds/FeedsJinpyeong';
 import SidebarContainer from './mainComponents/Sidebar/SidebarContainer/SidebarContainerJinpyeong';
 
 const MainJinpyeong = () => {
+  const [feedData, setFeedData] = useState([]);
   const storyArr = [];
   const recommendArr = [];
   for (let i = 0; i < 10; i++) {
     let randomStr = Math.random().toString(36).substring(2, 12);
     i < 5 ? storyArr.push(randomStr) : recommendArr.push(randomStr);
   }
+
+  useEffect(() => {
+    fetch('/data/feedDataJinpyeong.json', { method: 'GET' })
+      .then(response => response.json())
+      .then(data => setFeedData(data));
+  }, []);
+  console.log(feedData);
   return (
     <div className="mainJinpyeong">
       <Nav userList={[...storyArr, ...recommendArr]} />
